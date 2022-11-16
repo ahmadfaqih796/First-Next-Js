41a37e04d1065cea5e3da89e2ac35ffe34811dac
ahmadfaqih796 <ahmadfaqih796@gmail.com>
2022-11-16 16:52:54 +0700

    head

---
+++ pages/posts/head.js
@@ -0,0 +1,12 @@
+import Judul from "next/head"
+export default function Head() {
+  return (
+    <div>
+      <Judul>
+        <title>ini head</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Judul>
+      <p>Hello world!</p>
+    </div>
+  )
+}
