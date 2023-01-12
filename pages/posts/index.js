a6618d42659f7b098f4473cf1ab44f851831b68e
ahmadfaqih796 <ahmadfaqih796@gmail.com>
2023-01-12 10:45:11 +0700

    navbar

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,7 +1,28 @@
-export default function Posts() {
+import { useState } from "react";
+
+const Posts = () => {
+  const [name, setName] = useState("");
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+  const data = {
+    nama: name,
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const len = data.push(setName(e.target.value));
+    console.log(len, "inin dada");
+  };
+
+  console.log(data);
   return (
     <>
-      <h1>hello post</h1>
+      <form>
+        <input type="text" name="nama" value={name} onChange={handleName} />
+        <input type="submit" value={"submit"} onClick={handleSubmit} />
+      </form>
+      <h1>{name}</h1>
     </>
   );
-}
+};
+export default Posts;
